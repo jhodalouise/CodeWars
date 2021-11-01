256e7bfe0bfdb7ba3da78b77e5bc44e28fee3492
jhodalouise <jhodalouise@gmail.com>
2021-11-01 11:45:33 +0800

    7 kyu Kata

---
+++ regexValidatePINcode.py
@@ -0,0 +1,18 @@
+'''
+7 kyu Regex validate PIN code
+
+ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+If the function is passed a valid PIN string, return true, else return false.
+Examples (Input --> Output)
+
+"1234"   -->  true
+"12345"  -->  false
+"a234"   -->  false
+'''
+pin = "-12345"
+
+def validate_pin(pin):
+    return pin.isdecimal() and (len(pin) == 4 or len(pin) == 6)
+
+print(pin.isdecimal())
