2a6b48544194e4ca6bd6001866e2b212fca7b79d
jhodalouise <jhodalouise@gmail.com>
2021-11-28 15:50:05 +0800

    8 kyu KATA

---
+++ reversedStrings.py
@@ -0,0 +1,12 @@
+'''
+8 kyu - Reversed Strings
+Complete the solution so that it reverses the string passed into it.
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+'''
+string = 'world'
+
+def solution(string):
+    return string[::-1]
+
+print(solution(string))
